89cb8cec2712b6f91e7b48f519f51b73269dcbb5
Brandon Gray <brandonjoe88@hotmail.com>
2022-08-25 11:14:12 -0600

    Add test processes

--- variables.rb
+++ variables.rb
@@ -6,3 +6,11 @@ first_float = 1.1
 second_float = 2.2
 first_bool = true
 second_bool = false
+
+first_string = "first again"
+second_string = "second_again"
+
+puts first_integer
+puts second_integer
+puts first_bool
+puts second_float
